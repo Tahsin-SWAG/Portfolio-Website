183ca57fe5d16c28a60624933c9b40e218bb1c84
TahsinMahi <tahsintasif8@gmail.com>
2021-11-02 00:59:36 +0600

    I want to be a proggramer

---
+++ op.js
@@ -0,0 +1,11 @@
+const typed = new Typed('.test', {
+    strings: ['TAHSIN MAHI',
+     'Devloper',
+    'WEB DEVLOPER',
+    'PROGGRAMAR',
+    'DESIGNER' ],
+    typeSpeed:40,
+    backSpeed:40,
+    backdelay:1500,
+    loop:true,
+  }); 
